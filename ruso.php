a7507885b8e155e2182c74630cfb2536f6f3793a
uncover-id <uncover.all.86@gmail.com>
2024-05-09 18:25:38 +0700

    init

--- ruso.php
+++ ruso.php
@@ -1,10 +1,10 @@
-
-
 <?php
 
-date_default_timezone_set("Asia/Jakarta");
-
-session_start();
+// Allow from any origin
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -12,58 +12,23 @@ use PHPMailer\PHPMailer\SMTP;
 
 require './vendor/autoload.php';
 
-if (isset($_POST['name'])) {
+try {
 
     $name = $_POST['title'];
     $group = $_POST['group'];
     $pname = $_POST['name'];
     $email = $_POST['email'];
-    $start =
-        $_POST['start'];
-    $end =
-        $_POST['end'];
-    $body = `Halo ` . $pname . `!
-<br><br>
-Terimakasih telah mendaftar untuk turut berpartisipasi dalam acara ` . $name . `.
-<br><br>
-Detail Acara
-<br>
-` . $name . `
-Waktu: ` . $start . `<br><br>
 
-Silakan klik link di bawah ini untuk bergabung ke dalam grup yang telah kami sediakan, untuk informasi dan koordinasi acara lebih lanjut.  
-<br><br>` . $group . `<br><br>Terimakasih.`;
-
-    $wax = date("Y-m-d H:i:s", strtotime($start));
-    $way = date("Y-m-d H:i:s", strtotime($end));
-
-    $grip = date("Y-m-d", strtotime($wax)) . "T" . date("H:i:s", strtotime($wax));
-    $gripp = date("Y-m-d", strtotime($way)) . "T" . date("H:i:s", strtotime($way));
-    $eventStart = date_create($grip, new DateTimeZone('Asia/Jakarta'));
-    $eventEnd = date_create($gripp, new DateTimeZone('Asia/Jakarta'));
-
-    $icsContent = "BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-DTSTAMP:" . date('Ymd\THis') . "
-DTSTART:" . $eventStart->format('Ymd\THis') . "
-DTEND:" . $eventEnd->format('Ymd\THis') . "
-SUMMARY:" . $trn . " - Konseling
-DESCRIPTION:Jadwal dibuat oleh " . $fnm . " untuk ditangani oleh " . $trn . " perihal " . $tp . ".
-END:VEVENT
-END:VCALENDAR";
-
-    // Simpan konten .ics ke file
-    file_put_contents('./jadwal/' . $name . '.ics', $icsContent);
+    $body = "Halo " . $pname . "!<br><br>Terimakasih telah mendaftar untuk turut berpartisipasi dalam acara " . $name . ".<br><br>Silakan klik link di bawah ini untuk bergabung ke dalam grup yang telah kami sediakan, untuk informasi dan koordinasi acara lebih lanjut.<br><br>" . $group . "<br><br>Terimakasih.";
 
     $mail = new PHPMailer(true);
 
     // Konfigurasi SMTP
     $mail->isSMTP();
-    $mail->Host = 'mail.compasspubindonesia.com'; // Ganti dengan server SMTP Anda
+    $mail->Host = 'smtp.hostinger.com'; // Ganti dengan server SMTP Anda
     $mail->SMTPAuth = true;
     $mail->Username = 'cs@compasspubindonesia.com'; // Ganti dengan alamat email Anda
-    $mail->Password = 'CompassPub2024!'; // Ganti dengan kata sandi email Anda
+    $mail->Password = 'CompassId2024!'; // Ganti dengan kata sandi email Anda
     $mail->SMTPSecure = 'ssl'; // Gunakan 'tls' atau 'ssl' sesuai kebutuhan
     $mail->Port = 465; // Port SMTP
 
@@ -75,8 +40,9 @@ END:VCALENDAR";
     $mail->isHTML(true);
     $mail->Subject = $name . ": Registrasi Berhasil - " . $pname;
     $mail->Body = $body;
-    $mail->addAttachment($name . '.ics', $name . '.ics');
 
     // Kirim email
     $mail->send();
+} catch (Exception $e) {
+    print "Failed: " . $e->getMessage();
 }
