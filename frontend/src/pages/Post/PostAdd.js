dca081909f70f07aa9433793376645f741ae9856
uncover-id <uncover.all.86@gmail.com>
2024-05-14 14:55:27 +0700

    init

--- frontend/src/pages/Post/PostAdd.js
+++ frontend/src/pages/Post/PostAdd.js
@@ -28,8 +28,7 @@ function PostAdd() {
     const year = now.getFullYear();
     const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
-    const formattedDate = `-${day}-${month}-${year}-${hours}-${minutes}-${seconds}`;
+    const formattedDate = `-${day}-${month}-${year}-${hours}-${minutes}`;
 
     const cleanData = {
       ...postData,
@@ -66,6 +65,8 @@ function PostAdd() {
   };
 
   const AddPost = async (e) => {
+    document.getElementById("submit").innerText("Saving data, please wait...");
+    document.getElementById("submit").type = "reset";
     e.preventDefault();
 
     const cleanedData = {
--- frontend/src/pages/Post/PostEdit.js
+++ frontend/src/pages/Post/PostEdit.js
@@ -1,112 +1,135 @@
-// import dependencies
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
 function PostEdit() {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [eventData, setEventData] = useState({
-    price: "",
-    slug: "",
-    model: "",
-    name: "",
-    desc: "",
-    pic: "",
-    img: "",
-    address: "",
-    start: "",
-    end: "",
-    contact: "",
-  });
+  // Fetches latest Post count for serie generation (Optional)
 
   // get id from parameter
   const { id } = useParams();
 
-  // setting up useNavigate
+  const [selectedBanner, setSelectedBanner] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [postData, setPostData] = useState({
+    title: "",
+    slug: "",
+    category: "",
+    body: "",
+    date: "",
+    banner: "",
+    fileList: [],
+  });
+
+  // Setting up useNavigate
   const navigate = useNavigate();
 
   // create Event deleter function
-  const delEvent = async () => {
-    try {
-      await axios.delete(`https://seg-server.vercel.app/api/events/id/${id}`); // modify URL based on backend
-      // navigate to main page
-      navigate(`/events`);
-    } catch (error) {
-      console.log(error.message); // display error message
+  const delPost = async () => {
+    if (window.confirm("Delete this?") === true) {
+      try {
+        await axios.delete(`https://seg-server.vercel.app/api/posts/id/${id}`); // modify URL based on backend
+        // navigate to main page
+        navigate(`/posts`);
+      } catch (error) {
+        console.log(error.message); // display error message
+      }
+    } else {
     }
   };
 
-  // create Event update function
-  const updEvent = async (e) => {
-    e.preventDefault(); // Prevent default form submission
-    const slg = eventData.name.toLocaleLowerCase().split(" ").join("-");
-    const cleanedData = {
-      ...eventData,
-      slug: slg,
+  const handleChange = (e) => {
+    // For non-file inputs, set the value directly
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const formattedDate = `-${day}-${month}-${year}-${hours}-${minutes}`;
+
+    const cleanData = {
+      ...postData,
+      [e.target.name]: e.target.value,
+      slug:
+        postData.title.toLocaleLowerCase().split(" ").join("-") + formattedDate,
     };
 
-    const formData = new FormData();
-    formData.append("img", selectedFile);
+    setPostData(cleanData);
+  };
+
+  const handleBanner = (event) => {
+    const fileDir = "https://compasspubindonesia.com/media/api/posts/img/";
+    const file = event.target.files[0];
+    const filename = fileDir + file.name;
+    setSelectedBanner(file);
+    setPostData({
+      ...postData,
+      banner: filename,
+    });
+  };
+
+  const handleFile = (event) => {
+    const fileDir = "https://compasspubindonesia.com/media/api/posts/img/";
+    const files = Array.from(event.target.files);
+    const filenames = files.map((file) => ({
+      url: fileDir + file.name,
+    }));
+    setSelectedFile(files);
+    setPostData({
+      ...postData,
+      fileList: filenames,
+    });
+  };
+
+  const EditPost = async (e) => {
+    document.getElementById("submit").innerText("Updating...");
+    document.getElementById("submit").type = "reset";
+    e.preventDefault();
+
+    const cleanedData = {
+      ...postData,
+    };
+
+    const bannerData = new FormData();
+    bannerData.append("banner", selectedBanner);
+
+    const fileData = new FormData();
+    selectedFile.forEach((file, index) => {
+      fileData.append(`fileList[]`, file); // Append each file with `fileList[]` key
+    });
+
     try {
-      // Add the Event into database with axios
-      await axios.patch(
-        `https://seg-server.vercel.app/api/events/id/${id}`,
+      const response1 = await axios.patch(
+        `https://seg-server.vercel.app/api/posts/id/${id}`,
         cleanedData
       );
-      await axios.post(
-        `https://compasspubindonesia.com/media/api/events/index.php`,
-        formData,
+      console.log("Response from main API:", response1.data);
+
+      const response2 = await axios.post(
+        `https://compasspubindonesia.com/media/api/posts/banner.php`,
+        bannerData,
         {
           headers: {
             "Content-Type": "multipart/form-data",
           },
         }
       );
-      // Navigate to main page
-      navigate(`/events`);
+      console.log("Response from banner upload:", response2.data);
+
+      const response3 = await axios.post(
+        `https://compasspubindonesia.com/media/api/posts/files.php`,
+        fileData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("Response from file upload:", response3.data);
+
+      navigate(`/posts`);
     } catch (error) {
-      console.log(error); // display error message
-    }
-  };
-
-  // setting up useEffect to do tasks in real-time
-  useEffect(() => {
-    // create Event loader callback function
-    const getEventById = async () => {
-      try {
-        // get all the datas from database with axios
-        const res = await axios.get(
-          `https://seg-server.vercel.app/api/events/id/${id}`
-        );
-
-        // input all the datas into useState
-        setEventData(res.data);
-      } catch (error) {
-        console.log(error); // display error message
-      }
-    };
-
-    getEventById();
-  }, [id]);
-
-  const handleChange = (event) => {
-    setEventData({
-      ...eventData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleFile = (event) => {
-    if (event.target.files[0] !== null) {
-      setSelectedFile(event.target.files[0]);
-      // Access the filename from the selected file
-      const fileDir = "https://compasspubindonesia.com/media/api/events/img/";
-      const file = event.target.files[0];
-      const filename = fileDir + file.name;
-      setEventData({
-        ...eventData,
-        img: filename,
-      });
+      console.error("Error:", error.message);
     }
   };
 
@@ -114,140 +137,94 @@ function PostEdit() {
     <>
       <div className="section">
         <div className="section headline">
-          <h4>Edit Event</h4>
-          <button onClick={() => navigate(`/events`)} className="btn">
-            See All Events
+          <h4>Edit Post</h4>
+          <button onClick={() => navigate(`/posts`)} className="btn">
+            See All Posts
           </button>
         </div>
         <div className="section">
-          <form onSubmit={updEvent} className="form">
+          <form onSubmit={EditPost} className="form posti">
             <div className="field">
-              <label className="label">Name</label>
+              <label className="label">Title</label>
               <input
                 type="text"
                 className="input"
-                id="name"
-                name="name"
-                value={eventData.name}
+                id="title"
+                name="title"
+                value={postData.title}
                 onChange={handleChange}
-                placeholder="Event Name"
+                placeholder="Title"
               />
             </div>
             <div className="field">
-              <label className="label">Model</label>
+              <label className="label">Slug</label>
+              <input
+                type="text"
+                className="input"
+                id="slug"
+                name="slug"
+                value={postData.slug}
+                onChange={handleChange}
+                placeholder="Slug"
+              />
+            </div>
+            <div className="field">
+              <label className="label">Category</label>
               <select
-                name="model"
-                value={eventData.model}
+                name="category"
+                value={postData.category}
                 onChange={handleChange}
               >
-                <option value="">--- Select Model ---</option>
-                <option value="Online">Online</option>
-                <option value="Onsite">Onsite</option>
-                <option value="Hybrid (Online & Onsite)">Hybrid</option>
+                <option value="">--- Select Category ---</option>
+                <option value="Education">Education</option>
+                <option value="Book Review">Book Review</option>
+                <option value="Event">Event</option>
               </select>
             </div>
             <div className="field">
-              <label className="label">Start</label>
+              <label className="label">Date</label>
               <input
                 type="datetime-local"
                 className="input"
-                id="start"
-                name="start"
-                value={eventData.start}
+                id="date"
+                name="date"
+                value={postData.date}
                 onChange={handleChange}
-                placeholder="Start"
+                placeholder="Date"
               />
             </div>
             <div className="field">
-              <label className="label">End</label>
-              <input
-                type="datetime-local"
-                className="input"
-                id="end"
-                name="end"
-                value={eventData.end}
-                onChange={handleChange}
-                placeholder="End"
-              />
+              <label className="label">Banner (Cover Image for the Post)</label>
+              <input type="file" className="input" onChange={handleBanner} />
             </div>
             <div className="field">
-              <label className="label">Speaker(s)</label>
-              <input
-                type="text"
-                className="input"
-                id="pic"
-                name="pic"
-                value={eventData.pic}
-                onChange={handleChange}
-                placeholder="Speaker 1, Speaker 2, Speaker 3..."
-              />
-            </div>
-            <div className="field">
-              <label className="label">Price</label>
-              <input
-                type="number"
-                className="input"
-                id="price"
-                name="price"
-                value={eventData.price}
-                onChange={handleChange}
-                placeholder="Event Price in Rupiah"
-              />
-            </div>
-            <div className="field">
-              <label className="label">Contact Email/Phone</label>
-              <input
-                type="text"
-                className="input"
-                id="contact"
-                name="contact"
-                value={eventData.contact}
-                onChange={handleChange}
-                placeholder="Contact of Committee"
-              />
-            </div>
-            <div className="field">
-              <label className="label">Image</label>
+              <label className="label">Featured Images (Max. 10)</label>
               <input
                 type="file"
                 className="input"
-                id="img"
-                name="img"
+                multiple
                 onChange={handleFile}
-                placeholder="Event Image"
               />
             </div>
-            <div className="field">
-              <label className="label">Address</label>
+            <div className="field" style={{ width: "100%" }}>
+              <label className="label">Caption</label>
               <textarea
                 type="text"
                 className="input"
-                id="address"
-                name="address"
-                value={eventData.address}
+                id="body"
+                name="body"
+                value={postData.body}
                 onChange={handleChange}
-                placeholder="Event Address"
+                placeholder="Write captions here..."
+                style={{ height: "400px" }}
               ></textarea>
             </div>
-            <div className="field">
-              <label className="label">Description</label>
-              <textarea
-                type="text"
-                className="input"
-                id="desc"
-                name="desc"
-                value={eventData.desc}
-                onChange={handleChange}
-                placeholder="Event Description"
-              ></textarea>
-            </div>
-
             <div className="section">
               <div className="controls">
-                <button type="button" onClick={delEvent} className="btn">
+                <button type="button" onClick={delPost} className="btn">
                   Delete
                 </button>
-                <button type="submit" className="btn">
+                <button type="submit" className="btn" id="submit">
                   Update
                 </button>
               </div>
@@ -259,5 +236,4 @@ function PostEdit() {
   );
 }
 
-// export the main function
 export default PostEdit;
