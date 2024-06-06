36985f03a3321ee35630764462827e8ed24b6292
uncover-id <uncover.all.86@gmail.com>
2024-06-07 04:25:42 +0700

    init

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import errorHandler from "./config/error.js";
 
 // import routers
 import bookRouter from "./routes/BookRoute.js";
+import bookedRouter from "./routes/BookedRoute.js";
 import invoiceRouter from "./routes/InvoiceRoute.js";
 import quotationRouter from "./routes/QuotationRoute.js";
 import orderRouter from "./routes/OrderRoute.js";
@@ -28,6 +29,7 @@ app.use(
   cors(),
   errorHandler,
   bookRouter,
+  bookedRouter,
   invoiceRouter,
   quotationRouter,
   orderRouter,
---
+++ backend/models/BookedModel.js
@@ -0,0 +1,35 @@
+// import dependencies
+import mongoose from "mongoose";
+
+// create a schema
+const bookedSchema = mongoose.Schema(
+  {
+    src: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    isbn: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    bookPrice: {
+      type: String,
+    },
+    ebookPrice: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// export the schema
+export default mongoose.model("Booked", bookedSchema);
