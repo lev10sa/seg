a0b43055176dd46d64e57d028ed3224b2f5536a3
uncover-id <uncover.all.86@gmail.com>
2025-01-02 13:23:08 +0700

    init

--- frontend/src/pages/Event/EventPartyList.js
+++ frontend/src/pages/Event/EventPartyList.js
@@ -58,13 +58,13 @@ function EventPartyList() {
   const formatDate = (val) => {
     const date = new Date(val);
 
-const d = String(date.getDate()).padStart(2, '0');
-const m = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-const y = date.getFullYear();
-const hh = String(date.getHours()).padStart(2, '0');
-const mm = String(date.getMinutes()).padStart(2, '0');
+    const d = String(date.getDate()).padStart(2, "0");
+    const m = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+    const y = date.getFullYear();
+    const hh = String(date.getHours()).padStart(2, "0");
+    const mm = String(date.getMinutes()).padStart(2, "0");
 
-const exec = `${d}/${m}/${y}, ${hh}:${mm} WIB.`;
+    const exec = `${d}/${m}/${y}, ${hh}:${mm} WIB.`;
 
     return exec;
   };
@@ -184,7 +184,9 @@ const exec = `${d}/${m}/${y}, ${hh}:${mm} WIB.`;
                         </a>
                       </td>
                     ) : (
-                      ""
+                      <td>
+                        <a>No Attachment</a>
+                      </td>
                     )}
                     <td>
                       <button
